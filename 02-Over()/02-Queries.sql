4ce91058392224912e30746d5644a25600111bcb
lucodealethea <luc.vanrobays@hotmail.com>
2020-03-10 16:50:11 +0100

    Update 02-Queries.sql

--- 02-Over()/02-Queries.sql
+++ 02-Over()/02-Queries.sql
@@ -1,5 +1,6 @@
 '
-Typically, OVER() is used to compare the current row with an aggregate. For example, we can compute the difference between an employee salary and the average salary
+Typically, OVER() is used to compare the current row with an aggregate. For example, we can compute the difference 
+between an employee salary and the average salary
 '
 
 -- For each employee, find their first name, last name, salary and the sum of all salaries in the company.
@@ -14,7 +15,8 @@ first_name | last_name | salary |  sum  |
  Eugene     | Phillips  |   4877 | 94219 |
 
  
--- For each employee in table employee, select first and last name, years_worked, average of years spent in the company by all employees, and the difference between the years_worked and the average as difference 
+-- For each employee in table employee, select first and last name, years_worked, average of years spent in the company 
+-- by all employees, and the difference between the years_worked and the average as difference 
 SELECT first_name, last_name, years_worked,
   AVG(years_worked) OVER(),
   years_worked-AVG(years_worked) OVER() as difference
@@ -27,7 +29,8 @@ first_name | last_name | years_worked |        avg         |     difference     
  Eugene     | Phillips  |            2 | 3.3000000000000000 | -1.3000000000000000 |
 
  
- -- (CALCULATING PERCENTAGES) For all employees from department with department_id = 3, show their first_name, last_name, salary, the % of their salary to the SUM of all salaries in that department as percentage
+ -- (CALCULATING PERCENTAGES) For all employees from department with department_id = 3, 
+ -- show their first_name, last_name, salary, the % of their salary to the SUM of all salaries in that department as percentage
 SELECT first_name, last_name, salary,  
   (salary / SUM(salary) OVER())*100 as percentage
 FROM employee
@@ -43,7 +46,8 @@ first_name | last_name | salary |       percentage        |
  Keith      | Scott     |   4928 | 17.01833753496563870600 |
  
  
--- For each employee that earns more than 4000, show their first_name, last_name, salary and the number of all employees who earn more than 4000
+-- For each employee that earns more than 4000, show their first_name, last_name, salary and the number of all employees 
+-- who earn more than 4000
 select first_name, last_name, salary,
 	count(*) OVER() 
 from employee
@@ -55,19 +59,26 @@ first_name | last_name | salary | count |
  Eugene     | Phillips  |   4877 |    13 |
  Philip     | Mitchell  |   5259 |    13 |
  
-			-- For the above query, the normal query without window functions will be:
-			select first_name, last_name, salary, (select count(*) from employee where salary>4000)
-			from employee
-			where salary>4000
-			group by first_name, last_name, salary
+-- For the above query, the normal query without window functions will be:
+	select first_name, last_name, salary, (select count(*) from employee where salary>4000)
+	from employee
+	where salary>4000
+	group by first_name, last_name, salary
 
 
 '
-OVER() means "for all rows in the query result". This "in the query result" part is very important – window functions work only on the rows returned by the query.
+OVER() means "for all rows in the query result". This "in the query result" part is very important 
+   – window functions work only on the rows returned by the query.
 
-Here, this means we will get the salary of each IT department employee and the average salary in that department, and not in the entire company. That is a very important rule which we need to remember. Window functions are always executed AFTER the WHERE clause, so they work on whatever they find as the result.
+Here, this means we will get the salary of each IT department employee and the average salary in that department, 
+and not in the entire company. That is a very important rule which we need to remember. 
+Window functions are always executed AFTER the WHERE clause, so they work on whatever they find as the result.
 
-Now, it might be tempting to use window functions in a WHERE clause. However, when we run a query, we will get an error message. We cannot put window functions in WHERE. Why? The window functions are applied AFTER the rows are selected. If the window functions were in a WHERE clause, we would get a circular dependency: in order to compute the window function, we have to filter the rows with WHERE, which requires to compute the window function.
+Now, it might be tempting to use window functions in a WHERE clause. However, when we run a query, 
+we will get an error message. We cannot put window functions in WHERE. 
+Why? The window functions are applied AFTER the rows are selected. 
+If the window functions were in a WHERE clause, we would get a circular dependency: 
+in order to compute the window function, we have to filter the rows with WHERE, which requires to compute the window function.
 '
 
 
