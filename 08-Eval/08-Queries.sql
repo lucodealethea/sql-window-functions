9ce0c478f964c53eab7723f3ece9075249b2d903
lucodealethea <luc.vanrobays@hotmail.com>
2020-03-11 09:31:47 +0100

    Update 08-Queries.sql

--- 08-Eval/08-Queries.sql
+++ 08-Eval/08-Queries.sql
@@ -1,5 +1,6 @@
 '
-At the end of this part, we will compare window functions with other elements of SELECT queries. We will find out how to use window functions in various parts of the query and where we are not allowed to do so.
+At the end of this part, we will compare window functions with other elements of SELECT queries. 
+We will find out how to use window functions in various parts of the query and where we are not allowed to do so.
 '
 
 '
@@ -18,7 +19,9 @@ ORDER BY - the results are sorted,
 OFFSET - the first rows are skipped,
 LIMIT/FETCH/TOP - only the first rows are selected
 
-Practically, this order means that we cant put window functions anywhere in the FROM, WHERE, GROUP BY or HAVING clauses. This is because at the time of calculating these elements, window functions are not yet calculated - and it is impossible to use something which is not already available.
+Practically, this order means that we cant put window functions anywhere in the FROM, WHERE, GROUP BY or HAVING clauses. 
+This is because at the time of calculating these elements, window functions are not yet calculated - 
+and it is impossible to use something which is not already available.
 '
 
 -- Find the id, country and views for those auctions where the number of views was below the average.
@@ -41,7 +44,8 @@ select id, country, views from selection where views < average
  18 | UK      |    63 |
  
  
--- we would like to show those countries (country name and average final price) that have the average final price higher than the average price from all over the world. 
+-- we would like to show those countries (country name and average final price) that have the average final 
+-- price higher than the average price from all over the world. 
 SELECT country, AVG(final_price) 
 FROM auction 
 GROUP BY country 
@@ -54,7 +58,8 @@ HAVING AVG(final_price) > (Select AVG(final_price) from auction);
  UK      | 185.9633333333333333 |
  
  
--- divide all auctions into 6 equal groups based on the asking_price in ascending order. Show columns group_no, minimal, average and maximal value for that group. Sort by the group in ascending order.
+-- divide all auctions into 6 equal groups based on the asking_price in ascending order. 
+-- Show columns group_no, minimal, average and maximal value for that group. Sort by the group in ascending order.
 SELECT group_no, MIN(asking_price), avg(asking_price), MAX(asking_price)
 FROM
   (SELECT asking_price,
@@ -73,12 +78,15 @@ GROUP BY group_no;
 
 
 '
-To sum up this section, we just have to remember the following rule: the only places where we can use window functions without having to write subqueries are the SELECT and ORDER BY clauses. In all other places we have to use subqueries.
+To sum up this section, we just have to remember the following rule: the only places where we can use window functions 
+without having to write subqueries are the SELECT and ORDER BY clauses. In all other places we have to use subqueries.
 '
 
 
 '
-Before, we said that window functions were calculated after the GROUP BY clause. This has a very important implication for our queries: if the query uses any aggregates, GROUP BY or HAVING, the window function sees the group rows instead of the original table rows.
+Before, we said that window functions were calculated after the GROUP BY clause. This has a very important implication 
+for our queries: if the query uses any aggregates, GROUP BY or HAVING, the window function sees the group rows instead of 
+the original table rows.
 
 To get a better understanding of this phenomenon, take a look at the following example:
 '
@@ -93,9 +101,11 @@ SELECT category_id, MAX(final_price),
 FROM auction 
 GROUP BY category_id;
 >>>
-ERROR: column "auction.final_price" must appear in the GROUP BY clause or be used in an aggregate function Line: 3 Position in the line: 19
+ERROR: column "auction.final_price" must appear in the GROUP BY clause or be used in an aggregate function Line: 
+3 Position in the line: 19
 '
-This query does not work. This is because we cant use the column final_price in the window function. Once the rows have been grouped, there is no final_price value that makes sense for all the rows together.
+This query does not work. This is because we cant use the column final_price in the window function. 
+Once the rows have been grouped, there is no final_price value that makes sense for all the rows together.
 
 However, lets take a look at another modification of this example:
 '
@@ -112,7 +122,8 @@ GROUP BY category_id;
            3 |    242.16 | 223.2200000000000000 |
            2 |    197.43 | 223.2200000000000000 |
 '
-The query will now succeed because we used an aggregate function (MAX(final_price)) that was indeed available after grouping the rows. By the way, this is the only place where we can nest aggregate functions inside one another.
+The query will now succeed because we used an aggregate function (MAX(final_price)) that was indeed available after grouping the rows. 
+By the way, this is the only place where we can nest aggregate functions inside one another.
 
 The best way to correctly create queries with window functions and GROUP BY is as follows: 
 	First, create the query with GROUP BY, but without window functions. 
@@ -121,7 +132,8 @@ The best way to correctly create queries with window functions and GROUP BY is a
 '
 
 
--- Group the auctions by the country. Show the country, the minimal number of participants in an auction and the average minimal number of participants across all countries.
+-- Group the auctions by the country. Show the country, the minimal number of participants in an auction and 
+		-- the average minimal number of participants across all countries.
 SELECT country, MIN(participants),
 	AVG(MIN(participants)) OVER()
 FROM auction
@@ -152,7 +164,8 @@ GROUP BY country;
  France  |     4 |    3 |
  UK      |     3 |    5 |
 '
-We grouped auctions with respect to the country, counted the number of auctions from each country, and then we created a ranking based on that count of auctions.
+We grouped auctions with respect to the country, counted the number of auctions from each country, 
+		and then we created a ranking based on that count of auctions.
 '
 
 
@@ -175,7 +188,8 @@ ORDER BY ended
  
  
 '
-Finally, we can use window functions with PARTITION BY on grouped rows. One thing we need to remember is that the window function will only see grouped rows, not the original rows. For example:
+Finally, we can use window functions with PARTITION BY on grouped rows. One thing we need to remember is that the window function 
+		will only see grouped rows, not the original rows. For example:
 '
 SELECT country, ended, SUM(views) AS views_on_day,
 	SUM(SUM(views)) OVER(PARTITION BY country) AS views_country
@@ -191,7 +205,12 @@ ORDER BY country, ended
  Germany | 2017-01-05 |          267 |           610 |
  Germany | 2017-01-06 |          343 |           610 |
 '
-The query might require a bit of explanation. First of all, we grouped all rows by the country and ended. Then, we showed the country name and date when the auctions ended. Look what happens in the next two columns. First, we simply sum the views in accordance with our GROUP BY clause, i.e. we get the sum of views in all auctions from the particular country on the particular day. But look what happens next. We use a window function to sum all daily sums for a particular country. As a result, we get the sum of views for a particular country on all days.
+The query might require a bit of explanation. First of all, we grouped all rows by the country and ended. 
+		Then, we showed the country name and date when the auctions ended. Look what happens in the next two columns. 
+		First, we simply sum the views in accordance with our GROUP BY clause, i.e. we get the sum of views in all auctions 
+		from the particular country on the particular day. But look what happens next. 
+		We use a window function to sum all daily sums for a particular country. 
+		As a result, we get the sum of views for a particular country on all days.
 '
 
 
